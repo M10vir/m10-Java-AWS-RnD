bd5fb803ecffe3e920df070fb48f72d03ac7d984
M10vir <tanvirscsa@gmail.com>
2024-07-16 13:18:02 +0300

    sdf

---
+++ server_stop.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+echo  "Stopping the server/application"
+
+# Command to stop the server/application
+# For example, if using a Java application, you might use:
+# pkill -f 'java -jar your-application.jar'
+
+echo "Server/application stopped."
